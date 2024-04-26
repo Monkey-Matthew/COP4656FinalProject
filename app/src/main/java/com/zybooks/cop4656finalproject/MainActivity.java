3ed3dd957a72b12bfb193c1b0d8589fc3797a451
uravghypbeasrt <39703970+uravghypbeasrt@users.noreply.github.com>
2024-04-26 13:29:43 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 4eecf71b384876eb21baeeb46aa3c9094fbcff23, reversing
    changes made to 74f49abb997e4314e1e799f9f025300cb4aedf3b.

--- app/src/main/java/com/zybooks/cop4656finalproject/MainActivity.java
+++ app/src/main/java/com/zybooks/cop4656finalproject/MainActivity.java
@@ -13,17 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Finds the id of the welcome button to use later in the code.
+        // Find the button by its ID
         Button welcomeButton = findViewById(R.id.welcome_button);
 
-        //Sets onClickListener that will start the SecondActivity once it is clicked.
+        // Set OnClickListener for the button
         welcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent to start the new activity.
+                // Create an Intent to start the new activity
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
 
-                //Starts the new activity.
+                // Start the new activity
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/zybooks/cop4656finalproject/SecondActivity.java
+++ app/src/main/java/com/zybooks/cop4656finalproject/SecondActivity.java
@@ -1,66 +1,30 @@
 package com.zybooks.cop4656finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.eazegraph.lib.charts.PieChart;
-import org.eazegraph.lib.models.PieModel;
-
 public class SecondActivity extends AppCompatActivity {
-
-    //Creates two static ints that can be referenced in other activities and xml files.
-    public static int budgetSpent = 3000;
-    public static int budgetLeft = 2000;
-
-    //Creates the object pieChart class.
-    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        //This refrences the pieChartLayout so that we can have the layout act like a button.
-        LinearLayout pieChartLayout = findViewById(R.id.pieChartView);
+        // Find the TextView by its ID
+        TextView textView = findViewById(R.id.budgetCard);
 
-        pieChartLayout.setOnClickListener(new View.OnClickListener() {
+        // Set OnClickListener for the TextView
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecondActivity.this, PieChartActivity.class);
-                startActivity(intent);
+                // Handle click event here
+                // For example, you can show a Toast message
+
+                Toast.makeText(SecondActivity.this, "Div block clicked!", Toast.LENGTH_SHORT).show();
             }
         });
-
-        //Grabs the id for the pieChart so we can update how it looks.
-        pieChart = findViewById(R.id.piechart);
-
-        //Calls to setData for the pie chart.
-        setData();
-    }
-
-    private void setData()
-    {
-        //Sets the data and the colors to the pie chart.
-        pieChart.addPieSlice(
-                new PieModel(
-                        "BudgetLeft",
-                        budgetSpent,
-                        Color.parseColor("#000000")));
-        pieChart.addPieSlice(
-                new PieModel(
-                        "BudgentSpent",
-                        budgetLeft,
-                        Color.parseColor("#FF5722")));
-
-        //Animates the pie chart.
-        pieChart.startAnimation();
-
     }
 }
